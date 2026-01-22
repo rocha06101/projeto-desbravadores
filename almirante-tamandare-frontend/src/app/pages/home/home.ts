db011a395df79abe87e4161f69d0a8e6c6e62bf4
rocha06101 <ferpa.felipe2@gmail.com>
2026-01-22 03:41:56 -0300

    Subindo a tela home concluída

--- almirante-tamandare-frontend/src/app/pages/home/home.ts
+++ almirante-tamandare-frontend/src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SectionsComponent } from '../../shared/components/sections/sections';
 @Component({
@@ -9,4 +9,23 @@ import { SectionsComponent } from '../../shared/components/sections/sections';
 })
 export class Home {
 
+  @ViewChild('profilePopup') profilePopup!: ElementRef; 
+  @ViewChild('profileButton') profileButton!: ElementRef;
+  profileOpen: boolean = false;
+
+  toggleProfile(event: MouseEvent) {
+    event.stopPropagation();
+    this.profileOpen = !this.profileOpen;
+  }
+
+  @HostListener('document:click', ['$event']) closeOnclickOutside(event: MouseEvent) {
+    if(!this.profileOpen) return;
+
+    const clickedInseidePopup = this.profilePopup?.nativeElement.contains(event.target);
+    const clickedProfileButton = this.profileButton?.nativeElement.contains(event.target);
+
+    if (!clickedInseidePopup && !clickedProfileButton ) {
+      this.profileOpen = false;
+    }
+  }
 }
